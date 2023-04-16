f30ce4e56d33580f8fd74f2f020196befe978de7
HotelTango314 <henry.r.thomas-1@ou.edu>
2023-04-16 20:18:25 +0000

    initial commit-allesgut

---
+++ project2.py
@@ -0,0 +1,4 @@
+import numpy as np
+from sklearn.cluster import KMeans
+
+print("Hello, World!")
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='project2',
+        version='1.0',
+        author='Henry Thomas',
+        author_email = 'henry.r.thomas-1@ou.edu',
+        packages=find_packages(exclude=('tests','docs')),
+        setup_requires=['pytest-runner'],
+        tests_require=['pytest']
+    )
+
