8e0a786da55e605ada77151d174c2b38e8d10039
HotelTango314 <henry.r.thomas-1@ou.edu>
2023-04-16 22:20:06 +0000

    progress@EOD1

--- project2.py
+++ project2.py
@@ -1,4 +1,40 @@
 import numpy as np
 from sklearn.cluster import KMeans
+from sklearn.feature_extraction.text import TfidfVectorizer
+import argparse
+import pandas as pd
+from sklearn.datasets import fetch_20newsgroups
+import json
 
-print("Hello, World!")
+def recommender(N, ingredient):
+    #dataset = fetch_20newsgroups(
+    #        remove=("headers","footers","quotes"),
+    #        subset="all",
+    #        #categories=categories,
+    #        shuffle=True,
+    #        random_state=42
+    #        )
+    #print(dataset.target)
+    with open('docs/yummly.json','r') as f:
+        recipes = json.load(f)
+    print(recipes['cuisine'])
+    #rec_df = pd.DataFrame.from_dict(pd.json_normalize(recipes),orient='columns')
+    #total_list = []
+    #for x in rec_df['ingredients']:
+    #    for y in x:
+    #        total_list.append(y)
+    #nodup = [*set(total_list)]
+    #for i in nodup:
+    #    print(i)
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--N', required=True, help="Provide number of neighbors")
+    parser.add_argument('--ingredient', action='append')
+
+    args = parser.parse_args()
+    recommender(args.N, args.ingredient)
