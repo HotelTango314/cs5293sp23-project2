bd4cb836d814fa907337d67df6af36c7475085db
HotelTango314 <henry.r.thomas-1@ou.edu>
2023-04-19 03:03:58 +0000

    eod 4/18/2023

--- project2.py
+++ project2.py
@@ -1,25 +1,53 @@
 import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import Normalizer
+from sklearn import metrics
 import argparse
 import pandas as pd
-from sklearn.datasets import fetch_20newsgroups
 import json
+from collections import Counter
+
+def list_to_sent(lst):
+    text = ''
+    for i in lst:
+        text = text + i + ', '
+    return text
+
 
 def recommender(N, ingredient):
     with open('docs/yummly.json','r') as f:
         recipes = json.load(f)
-    print(recipes['cuisine'])
     rec_df = pd.DataFrame.from_dict(pd.json_normalize(recipes),orient='columns')
-    total_list = []
+    X = []
+    y = []
     for x in rec_df['ingredients']:
-        for y in x:
-            total_list.append(y)
-    nodup = [*set(total_list)]
-    for i in nodup:
-        print(i)
+        X.append(list_to_sent(x))
+    for z in rec_df['cuisine']:
+        y.append(z)
+    
+    vectorizer = TfidfVectorizer(max_df = .75, min_df = 5, stop_words="english")
+    X_tfidf = vectorizer.fit_transform(X)
+    
+    lsa = make_pipeline(TruncatedSVD(n_components = 100), Normalizer(copy=False))
+    X_lsa = lsa.fit_transform(X_tfidf)
+    kmeans = KMeans(n_clusters=20, max_iter=100, n_init=1)
+    kmeans.fit(X_lsa)
+    
+    cluster_label = [[] for _ in range(20)]
+    cluster_dict = dict()
+    for x in range(len(kmeans.labels_)):
+        cluster_label[kmeans.labels_[x]].append(y[x])
+    for x in range(len(cluster_label)):
+        c = Counter(cluster_label[x])
+        value, count = c.most_common()[0]
+        cluster_dict[x] = value
+    
 
 
+    
 
 
 if __name__ == '__main__':
